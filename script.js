5442f215d41fdc5c7dc71cf9d89fea206a649ad9
BarkMulcher <lucas.c.haskell@gmail.com>
2022-08-13 15:35:37 -0400

    Updated JS API functions

--- script.js
+++ script.js
@@ -1,62 +1,76 @@
-// api URL: api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=30d6578d03fd53edd07b515697cb941a
-// unlock this later:
-// var queryFormEl = document.getElementById('')
-// get the modal:
-var modal = document.getElementById('myModal');
-// get button that opens modal - modify later to only open when form is blank!!
-var submitBtn = document.getElementById('submit-query');
-// get the <span> element that closes the modal:
-var closingSpan = document.getElementsByClassName('close')[0];
-// script the real button click handler way below later
-    // preliminary button handler open modal function:
-    var prelimBtnClkHandler = function() {
-        modal.style.display = 'block';
-    }
-    // add eventListener for closing modal button <span> (x)
-    closingSpan.addEventListener('click', prelimClsBtnClkHandler);
-    // preliminary btn handler close modal function:
-    var prelimClsBtnClkHandler = function() {
-        modal.style.display = 'none';
-    }
-    // allow click-off to close modal:
-    var clickOffCloseModal = function(event) {
-        if (event.target == modal) {
-            modal.style.display = 'none';
-        }
-    }
+// api URL example: api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=30d6578d03fd53edd07b515697cb941a
 
-// add eventListeners for clicking modal button
-submitBtn.addEventListener('click', prelimBtnClkHandler);
-closingSpan.addEventListener('click', prelimClsBtnClkHandler);
-window.addEventListener('click', clickOffCloseModal);
-// fkn hell yeah
-var getForecastResults = function(query) {
-    var forecastApiUrl = "http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=30d6578d03fd53edd07b515697cb941a";
+let queryFormEl = document.querySelector('#search-form');
+let queryInputEl = document.querySelector('#search-input');
+// modal declarations
+let modal = document.getElementById('myModal');
+let submitBtn = document.getElementById('submit-query');
+let closingSpan = document.getElementsByClassName('close')[0];
+// let searchParamsArr = document.location.search.split('//'); //some character here that delineates between lat/long :()
+// let query = searchParamsArr
 
 
-    fetch('apiUrletcetc.com"=${apiKey}');
+// one function to fetch location
+// triggers 2nd function to pull location data
+// prof3ssorSt3v3 on gist.github
+// 
+// "city" parameter to be named later
+var getWeatherResults = function(query) {
+    let lat = document.getElementById('latitude').value; // maybe?
+    let lon = document.getElementById('longitude').value; // maybe?
+    var forecastApiUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + lat + '&lon=' + lon + '&exclude=minutely&appid=30d6578d03fd53edd07b515697cb941a';
+    
+// enter the lat/lon in / '' + lat/lon + '' / format
+
+    fetch(forecastApiUrl)
         .then(function (response) {
-   
-        }
-)}
+            if (!response.ok) {
+                throw response.json();
+            }
 
-var currentWeatherResults = function(query) {
+            return response.json();
+        })
+    getCoordinates
+}
+
+var getCoordinates = function() {
+    var geocodeApiUrl = 'https://api.openweathermap.org/geo/1.0/direct?q=' + city + '&limit=5&appid=30d6578d03fd53edd07b515697cb941a';
+    
 
 }
 
+// var currentWeatherResults = function(query) {}
 
-// REVISE THIS LATER TO INCLUDE MODAL FOR EMPTY FORM
-// var buttonClickHandler = function (event) {
-//     var language = event.target.getAttribute('data-language');
+
+// REVISE THIS LATER TO INCLUDE the above MODAL FOR EMPTY FORM
+function buttonClickHandler(event) {
+    event.preventDefault();
+
+    var userInput = document.querySelector("#search-input").value;
+
+    if (!userInput) {
+        //Modal variables at top
+        console.error('do the thing');
+        modal.style.display = 'block';
+        return;
+    } else {
+        
+    }
+}
+var closeBtnClkHandler = function() {
+    modal.style.display = 'none';
+    
+}
+var clickOffCloseModal = function(event) {
+    if (event.target == modal) {
+            modal.style.display = 'none';
+    }
+        
+}
+// fkn hell yeah ^
+
+closingSpan.addEventListener('click', closeBtnClkHandler);
+submitBtn.addEventListener('click', buttonClickHandler);
+window.addEventListener('click', clickOffCloseModal);
+    
   
-//     if (!query) {
-//         //Modal variables at top
-//         //Modal script here:
-
-//     }
-  
-//       repoContainerEl.textContent = '';
-//     }
-//   };
-
-// queryFormEl.addEventListener('submit', searchFormSubmitHandler);
