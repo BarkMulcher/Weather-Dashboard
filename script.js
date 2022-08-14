b1dadede369a3eaecbe6156a1a31d33fa7d56209
BarkMulcher <lucas.c.haskell@gmail.com>
2022-08-14 18:52:46 -0400

    javascript update

--- script.js
+++ script.js
@@ -2,23 +2,22 @@
 
 let queryFormEl = document.querySelector('#search-form');
 let queryInputEl = document.querySelector('#search-input');
-let weatherResultsEl = document.querySelector('#current-conditions');
+let weatherResultsEl = document.querySelector('#current-conditions-container');
 let query = document.querySelector("#search-input").value;
+let userInputEl = document.querySelector('#searched-city');
 let searchHistoryBtnsEl = document.querySelector('#search-history-buttons');
 let forecastResultsEl = document.querySelector('#fiver-container');
 // modal declarations
 let modal = document.getElementById('myModal');
 let submitBtn = document.getElementById('submit-query');
 let closingSpan = document.getElementsByClassName('close')[0];
-// let weatherIconUrl = 'https://'
-// let searchParamsArr = document.location.search;
-// let query = searchParamsArr
-
 const oneCallAPI = 'https://api.openweathermap.org/data/2.5/onecall?';
 const geocodeApiUrl = 'https://api.openweathermap.org/geo/1.0/direct?q=';
 const apiKey = '30d6578d03fd53edd07b515697cb941a';
 let history = JSON.parse(localStorage.getItem('query')) || [];
 console.log(history);
+const searchHistory = document.getElementById('search-history-buttons');
+
 
 // click handler function with modal:
 function buttonClickHandler(event) {
@@ -36,14 +35,14 @@ function buttonClickHandler(event) {
         //getFiver(query);
         queryInputEl.value = '';
     }
-    
-    getWeatherResults(query);
+    saveSearch();
 }
-
+ // modal close fn
 var closeBtnClkHandler = function() {
     modal.style.display = 'none';
     
 }
+// modal close fn
 var clickOffCloseModal = function(event) {
     if (event.target == modal) {
             modal.style.display = 'none';
@@ -51,62 +50,95 @@ var clickOffCloseModal = function(event) {
         
 }
 
-// one function to fetch location
-// triggers 2nd function to use location data?
-// 
-var getWeatherResults = function(query) {
-    // let lat = document.getElementById('latitude').value; // saving these for later
-    // let lon = document.getElementById('longitude').value; // saving these for later
-    // var forecastApiUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + lat + '&lon=' + lon + '&exclude=minutely&appid=30d6578d03fd53edd07b515697cb941a'; // saving for later
-    var currentForecastApiUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + query + '&appid=' + apiKey + '&units=imperial';
-    
-    
-// enter the lat/lon in / '' + lat/lon + '' / format
+var saveSearch = function () {
+    localStorage.setItem('history', JSON.stringify(history));
+} 
 
-    fetch(currentForecastApiUrl)
-        .then(function (response) {
-            
-            if (response.ok) {
-                console.log(response);
-                response.json()
-                .then(function(data){
-                    console.log(data);               
-                displayCurrent(data, query);
-                });
-            } else {
-                console.log('error: ' + response.statusText);
-            }
+var getLatLon = function(query) {
+    
+    fetch(geocodeApiUrl + query + '&limit=3&appid=' + apiKey)
+    .then(function (response) {
+        if (response.ok) {
+            console.log(response);
+            return response.json();
+        }})
+    
+        .then(function (data) {
+            console.log(data);
         })
-            
-            
-        // const tempF = response.weather.main;
-        //     var weatherIcon = weather.icon;
-        //     var weatherIconURL = 'https://openweathermap.org/img/wn/' + weatherIcon + '@2x.png'
-        // .then(function (weatherResults) {
-        //     weatherResultsEl.textContent = weatherResults.query;
+        var lat = data.coord.lat;
+        var lon = data.coord.lon;
 
-        //     console.log(weatherResults);
-        //       }
-        
-    // getCoordinates
+
+
 }
-// display current weather - just test - going to meet criteria after test
-var displayCurrent = function (query, userInput) {
+
+
+
+var getWeatherResults = function(query) {
+    var forecastApiUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + lat + '&lon=' + lon + '&exclude=minutely,hourly&appid=30d6578d03fd53edd07b515697cb941a';
+
+    
+    fetch(forecastApiUrl)
+        .then(function (response) {
+            if (response.ok) {
+            console.log(response);
+            return response.json();
+        }})
+            .then(function (data) {
+                console.log(data);
+                displayCurrent(data, query);
+            })  
+            var lat = data.coord.lat;
+            var lon = data.coord.lon; 
+            
+}// var currentForecastApiUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + query + '&appid=' + apiKey + '&units=imperial';
+
+// display current weather 
+var displayCurrent = function (data, query) {
     query = document.querySelector("#search-input").value;
     weatherResultsEl.textContent = '';
-    queryInputEl.textContent = userInput;
+
     
     // date element:
     let currentDate = document.createElement('span');
-    currentDate.textContent = ' (' + moment().format("(MM/DD/YYYY)");
+    currentDate.textContent = moment().format("MM/DD/YYYY");
+    userInputEl.appendChild(currentDate);
 
     // weather Icon image element:
-    console.log(data);
-    let weatherIcon = document.createElement('img');
-    let weatherIconVal = data.weather[0].icon;
-    weatherIcon.setAttribute('src', 'https://openweathermap.org/img/wn/' + weatherIconVal + '@2x.png');
-    queryInputEl.classList = 'list-group-item';
+    let weatherImg = document.createElement('img');
+    let weatherImgVal = data.weather[0].icon;
+    weatherImg.setAttribute('src', 'https://openweathermap.org/img/wn/' + weatherImgVal + '@2x.png');
+    userInputEl.appendChild(weatherImg);
+
+    // temperature
+    var currentTempEl = document.createElement('span');
+    currentTempEl.textContent = 'Current Temperature: ' + data.main.temp + ' degrees F'
+
+    // humidity section:
+    var currentHumidityEl = document.createElement('span');
+    currentHumidityEl.textContent = 'Humidity: ' + data.main.humidity + '%';
+    currentHumidityEl.classList = 'list-group-item';
+
+    // wind span
+    var windEl = document.createElement('span');
+    windEl.textContent = 'Wind Speed: ' + data.wind.speed + 'MPH';
+    windEl.classList = 'list-group-item';
+
+    weatherResultsEl.appendChild(currentTempEl);
+    weatherResultsEl.appendChild(currentHumidityEl);
+    weatherResultsEl.appendChild(windEl);
+
+
 }      
+
+
+
+
+
+
+
+
 // do this later:
 // var getCoordinates = function() {
 //     var geocodeApiUrl = 'https://api.openweathermap.org/geo/1.0/direct?q=' + city + '&limit=5&appid=30d6578d03fd53edd07b515697cb941a';
